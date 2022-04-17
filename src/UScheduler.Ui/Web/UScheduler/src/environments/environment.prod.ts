20353a39bb117588d25e7660e4ccb66647e34a10
Iulian Peiu <iulianpeiu6@gmail.com>
2022-04-17 17:23:54 +0300

    Add environments & auth0 config

---
+++ src/UScheduler.Ui/Web/UScheduler/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+import authConfig from "../../auth-config.json"
+
+export const environment = {
+  production: true,
+  auth: {
+    domain: authConfig.domain,
+    clientId: authConfig.clientId,
+    redirectUri: window.location.origin
+  }
+};
---
+++ src/UScheduler.Ui/Web/UScheduler/src/environments/environment.ts
@@ -0,0 +1,10 @@
+import authConfig from "../../auth-config.json"
+
+export const environment = {
+  production: false,
+  auth: {
+    domain: authConfig.domain,
+    clientId: authConfig.clientId,
+    redirectUri: window.location.origin
+  }
+};
